19a54cef80f6c93fd3e3f26e3d185250fb632029
YooInKeun <keun0390@naver.com>
2023-08-26 15:44:35 +0900

    add part1 props study

---
+++ src/routes/part1/props/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+    import Nested from "./nested.svelte";
+    import PacakgeInfo from "./PacakgeInfo.svelte";
+
+    const pkg = {
+        name: 'svelte',
+        speed: 'blazing',
+        version: 4,
+        website: 'htps://svelte.dev'
+    };
+</script>
+
+<Nested answer={42} />
+<Nested />
+
+<PacakgeInfo {...pkg}/>
