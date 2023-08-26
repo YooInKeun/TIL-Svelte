cb6ca0cd0137580f2936621302087db4c4de6124
YooInKeun <keun0390@naver.com>
2023-08-26 15:44:21 +0900

    add part1 reactivity study

---
+++ src/routes/part1/reactivity/+page.svelte
@@ -0,0 +1,48 @@
+<h1>Welcome to {name.toUpperCase()}</h1>
+<img {src} alt="favicon">
+<p class="paragraph">This is paragraph.</p>
+<Nested />
+<button on:click={increment}>
+    Clicked {count}
+    {count == 0 ? 'time' : 'times'}
+</button>
+
+<p>{count} doubled is {doubled}</p>
+
+<button on:click={addNumbers}>Add Number</button>
+<p>numbers is {numbers}</p>
+
+<script>
+    import Nested from "./nested.svelte";
+
+    let name = 'SvelteKit'
+    let src = "/favicon.png"
+
+    let count = 0;
+    let numbers = [1, 2, 3, 4];
+
+    $: doubled = count * 2;
+    
+    $: console.log(`the count is ${count}`);
+    $: if (count >= 10) {
+        alert('count is dangerously high!');
+        count = 0;
+    }
+
+    const increment = () => {
+        count += 1;
+    }
+
+    const addNumbers = () => {
+        numbers.push(numbers.length + 1);
+        numbers = numbers
+    }
+</script>
+
+<style>
+    .paragraph {
+        color: goldenrod;
+        font-family: 'Comics Sans MS', cursive;
+        font-size: 2em;
+    }
+</style>
