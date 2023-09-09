fff2369e60ce4f450824edd5af402530ca371e03
YooInKeun <keun0390@naver.com>
2023-09-09 19:53:29 +0900

    add life cycle study

--- src/routes/part1/life-cycle/+page.svelte
+++ src/routes/part1/life-cycle/+page.svelte
@@ -1,6 +1,7 @@
 <script>
     import { paint } from './gradient.js'
-    import { onMount } from 'svelte';
+    import { afterUpdate, beforeUpdate, onMount } from 'svelte';
+    import { tick } from 'svelte';
 
     onMount(() => {
         const canvas = document.querySelector('canvas');
@@ -11,11 +12,49 @@
             paint(context, t);
         });
     });
+    
+    beforeUpdate(() => {
+        console.log("beforeUpdate");
+    })
+
+    afterUpdate(() => {
+        console.log("afterUpdate");
+    })
+
+    let text = `Select some text and hit the tab key to toggle uppercase`;
+
+    async function handleKeydown(event) {
+        if (event.key !== 'Tab') return;
+
+        event.preventDefault();
+
+        const { selectionStart, selectionEnd, value } = this;
+        const selection = value.slice(selectionStart, selectionEnd);
+
+        const replacement = /[a-z]/.test(selection)
+            ? selection.toUpperCase()
+            : selection.toLowerCase();
+
+        text =
+            value.slice(0, selectionStart) +
+            replacement +
+            value.slice(selectionEnd);
+
+        // this has no effect, because the DOM hasn't updated yet
+        // await tick();
+        this.selectionStart = selectionStart;
+        this.selectionEnd = selectionEnd;
+    }
 </script>
 
 <canvas
     width={32}
     height={32}
+/>
+
+<textarea
+	value={text}
+	on:keydown={handleKeydown}
 />
 
 <style>
@@ -31,4 +70,10 @@
 		-webkit-mask: url(./svelte-logo-mask.svg) 50% 50% no-repeat;
 		-webkit-mask-size: 60vmin;
     }
+
+    textarea {
+		width: 100%;
+		height: 100%;
+		resize: none;
+	}
 </style>
